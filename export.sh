733d1c764627006d2dd569fb895f33359f9fac38
Dean Mercado <dean@yeti.co>
2018-07-18 10:08:48 -0700

    Encapsulate both shell scripts in functions

--- export.sh
+++ export.sh
@@ -1,27 +1,29 @@
 #! /bin/bash 
 
-NAME=$1
+function exportMachine () {
 
-if [ -z "$NAME" ]; then 
-  echo "machine-export <machine-name>"
-  exit 1
-fi
+    NAME=$1
 
-echo "exporting $NAME"
+    if [ -z "$NAME" ]; then 
+        echo "exportMachine <machine-name>"
+        exit 1
+    fi
 
-# stay clean
-rm -rf /tmp/$NAME
+    echo "exporting $NAME"
 
-# save a copy of the machine data
-cp -r $HOME/.docker/machine/machines/$NAME /tmp/$NAME
+    # stay clean
+    rm -rf /tmp/$NAME
 
-# stub out the host specific vars
-cat /tmp/$NAME/config.json | sed -e "s:$HOME:{{HOME}}:g" > /tmp/$NAME/config.json.stub
-mv /tmp/$NAME/config.json.stub /tmp/$NAME/config.json 
+    # save a copy of the machine data
+    cp -r $HOME/.docker/machine/machines/$NAME /tmp/$NAME
 
-# make a zip
-zip -r -j $NAME.zip /tmp/$NAME
+    # stub out the host specific vars
+    cat /tmp/$NAME/config.json | sed -e "s:$HOME:{{HOME}}:g" > /tmp/$NAME/config.json.stub
+    mv /tmp/$NAME/config.json.stub /tmp/$NAME/config.json 
 
-# clean up
-rm -rf /tmp/$NAME
+    # make a zip
+    zip -r -j $NAME.zip /tmp/$NAME
 
+    # clean up
+    rm -rf /tmp/$NAME
+}
--- import.sh
+++ import.sh
@@ -1,30 +1,30 @@
-#! /bin/bash 
+#!/bin/bash 
 
-TARGET=$1
-filename=$(basename "$TARGET")
-FILENAME="${filename%.*}"
+function importMachine () {
 
-if [ -d "$HOME/.docker/machine/machines/$FILENAME" ] ; then
-  echo "that machine already exists"
-  exit 1
-fi
+    TARGET=$1
+    filename=$(basename "$TARGET")
+    FILENAME="${filename%.*}"
 
-# cleanup
-rm -r /tmp/$FILENAME
+    if [ -d "$HOME/.docker/machine/machines/$FILENAME" ] ; then
+        echo "that machine already exists"
+        exit 1
+    fi
 
-# extract
-unzip $TARGET -d /tmp/$FILENAME
+    # cleanup
+    rm -r /tmp/$FILENAME
 
-# add correct $HOME var
-cat /tmp/$FILENAME/config.json | sed -e "s:{{HOME}}:$HOME:g" > /tmp/$FILENAME/config.json.fixed
-mv /tmp/$FILENAME/config.json.fixed /tmp/$FILENAME/config.json
+    # extract
+    unzip $TARGET -d /tmp/$FILENAME
 
-mkdir -p $HOME/.docker/machine/machines/$FILENAME
+    # add correct $HOME var
+    cat /tmp/$FILENAME/config.json | sed -e "s:{{HOME}}:$HOME:g" > /tmp/$FILENAME/config.json.fixed
+    mv /tmp/$FILENAME/config.json.fixed /tmp/$FILENAME/config.json
 
-# move it into docker machines files
-cp -r /tmp/$FILENAME $HOME/.docker/machine/machines/
+    mkdir -p $HOME/.docker/machine/machines/$FILENAME
 
-# update the stupid raw driver
-machine-driverfix $FILENAME
+    # move it into docker machines files
+    cp -r /tmp/$FILENAME $HOME/.docker/machine/machines/
+    echo "ok!"
+}
 
-echo "ok!"
